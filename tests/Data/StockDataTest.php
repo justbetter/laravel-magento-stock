b0de47dca04a956cb3ddd8be8432aabcd51c8e32
Vincent Boon <vincent@justbetter.nl>
2023-02-20 11:56:45 +0100

    Improve stock equal checks

--- tests/Data/StockDataTest.php
+++ tests/Data/StockDataTest.php
@@ -17,4 +17,137 @@ class StockDataTest extends TestCase
         $this->assertEquals(['A' => 10], $data->msiQuantity);
         $this->assertEquals(['A' => false], $data->msiStatus);
     }
+
+    /**
+     * @dataProvider msiStockDataProvider
+     */
+    public function test_it_compares_msi_stock(StockData $a, StockData $b, bool $equals): void
+    {
+        config()->set('magento-stock.msi', true);
+
+        $this->assertEquals(
+            $equals,
+            $a->equals($b)
+        );
+    }
+
+    protected function msiStockDataProvider(): array
+    {
+        return [
+            'Unchanged' => [
+                'a' => new StockData(
+                    sku: '::sku::',
+                    msiQuantity: [
+                        'A' => 5,
+                        'B' => 0,
+                        'C' => 0,
+                    ],
+                    msiStatus: [
+                        'A' => true,
+                        'B' => true,
+                        'C' => false,
+                    ]
+                ),
+                'b' => new StockData(
+                    sku: '::sku::',
+                    msiQuantity: [
+                        'A' => 5,
+                        'B' => 0,
+                        'C' => 0,
+                    ],
+                    msiStatus: [
+                        'A' => true,
+                        'B' => true,
+                        'C' => false,
+                    ]
+                ),
+                'equals' => true,
+            ],
+            'Quantity change' => [
+                'a' => new StockData(
+                    sku: '::sku::',
+                    msiQuantity: [
+                        'A' => 5,
+                        'B' => 2,
+                        'C' => 0,
+                    ],
+                    msiStatus: [
+                        'A' => true,
+                        'B' => true,
+                        'C' => false,
+                    ]
+                ),
+                'b' => new StockData(
+                    sku: '::sku::',
+                    msiQuantity: [
+                        'A' => 5,
+                        'B' => 0,
+                        'C' => 0,
+                    ],
+                    msiStatus: [
+                        'A' => true,
+                        'B' => true,
+                        'C' => false,
+                    ]
+                ),
+                'equals' => false,
+            ],
+            'Status change' => [
+                'a' => new StockData(
+                    sku: '::sku::',
+                    msiQuantity: [
+                        'A' => 5,
+                        'B' => 0,
+                        'C' => 0,
+                    ],
+                    msiStatus: [
+                        'A' => true,
+                        'B' => false,
+                        'C' => false,
+                    ]
+                ),
+                'b' => new StockData(
+                    sku: '::sku::',
+                    msiQuantity: [
+                        'A' => 5,
+                        'B' => 0,
+                        'C' => 0,
+                    ],
+                    msiStatus: [
+                        'A' => true,
+                        'B' => true,
+                        'C' => false,
+                    ]
+                ),
+                'equals' => false,
+            ],
+            'Count change' => [
+                'a' => new StockData(
+                    sku: '::sku::',
+                    msiQuantity: [
+                        'A' => 5,
+                        'B' => 0,
+                    ],
+                    msiStatus: [
+                        'A' => true,
+                        'B' => true,
+                    ]
+                ),
+                'b' => new StockData(
+                    sku: '::sku::',
+                    msiQuantity: [
+                        'A' => 5,
+                        'B' => 0,
+                        'C' => 0,
+                    ],
+                    msiStatus: [
+                        'A' => true,
+                        'B' => true,
+                        'C' => false,
+                    ]
+                ),
+                'equals' => false,
+            ],
+        ];
+    }
 }
