8398929b124bc06eec1f538cb2ea6758ce58d268
Vincent Boon <vincent@justbetter.nl>
2024-10-10 11:57:22 +0200

    Test

--- tests/Jobs/Comparison/DispatchComparisonsJobTest.php
+++ tests/Jobs/Comparison/DispatchComparisonsJobTest.php
@@ -4,6 +4,7 @@ namespace JustBetter\MagentoStock\Tests\Jobs\Comparison;
 
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Testing\Fakes\PendingBatchFake;
+use JustBetter\MagentoProducts\Models\MagentoProduct;
 use JustBetter\MagentoStock\Jobs\Comparison\CompareStockJob;
 use JustBetter\MagentoStock\Jobs\Comparison\DispatchComparisonsJob;
 use JustBetter\MagentoStock\Models\Stock;
@@ -18,7 +19,16 @@ class DispatchComparisonsJobTest extends TestCase
         Bus::fake([CompareStockJob::class]);
 
         Stock::query()->create([
-            'sku' => '::sku::',
+            'sku' => '::sku_1::',
+        ]);
+
+        Stock::query()->create([
+            'sku' => '::sku_2::',
+        ]);
+
+        MagentoProduct::query()->create([
+            'sku' => '::sku_1::',
+            'exists_in_magento' => true,
         ]);
 
         DispatchComparisonsJob::dispatch();
